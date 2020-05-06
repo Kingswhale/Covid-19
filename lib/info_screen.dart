5516cf7a9bc7df6822a76b66075040e2266d855f
Afeez Aderemi <hardeyreemyarfyc@gmail.com>
2020-05-06 16:48:20 +0100

    Initialize commit

---
+++ lib/info_screen.dart
@@ -0,0 +1,113 @@
+import 'package:covid_19/constant.dart';
+import 'package:covid_19/widgets/my_header.dart';
+import 'package:covid_19/widgets/prevent_card.dart';
+import 'package:covid_19/widgets/symtoms_card.dart';
+import 'package:flutter/material.dart';
+
+class InfoScreen extends StatefulWidget {
+  @override
+  _InfoScreenState createState() => _InfoScreenState();
+}
+
+class _InfoScreenState extends State<InfoScreen> {
+  final controller = ScrollController();
+  double offset = 0;
+     final int isCurrent = 1;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller.addListener(onScroll);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    controller.dispose();
+    super.dispose();
+  }
+
+  void onScroll() {
+    setState(() {
+      offset = (controller.hasClients) ? controller.offset : 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        controller: controller,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            MyHeader(
+              image: "assets/icons/coronadr.svg",
+              textTop: "Get to know",
+              textBottom: "About Covid-19.",
+              offset: offset,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Symptoms",
+                    style: kTitleTextstyle,
+                  ),
+                  SizedBox(height: 20),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        SymptomCard(
+                          image: "assets/images/headache.png",
+                          title: "Headache",
+                        ),
+                        SymptomCard(
+                          image: "assets/images/caugh.png",
+                          title: "Caugh",
+                        ),
+                        SymptomCard(
+                          image: "assets/images/fever.png",
+                          title: "Fever",
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Text("Prevention", style: kTitleTextstyle),
+                  SizedBox(height: 20),
+                  PreventCard(
+                    text:
+                        "Since the start of the coronavirus outbreak some places have fully embraced wearing facemasks",
+                    image: "assets/images/wear_mask.png",
+                    title: "Wear face mask",
+                  ),
+                  PreventCard(
+                    text:
+                        "Since the start of the coronavirus outbreak some places have fully embraced wearing facemasks",
+                    image: "assets/images/wash_hands.png",
+                    title: "Wash your hands",
+                  ),
+                  PreventCard(
+                    text:
+                        "Since the start of the coronavirus outbreak some places have fully embraced wearing facemasks",
+                    image: "assets/images/wash_hands.png",
+                    title: "Social Distancing",
+                  ),
+                  SizedBox(height: 50),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
